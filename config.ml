c083a9a20a2322ddd13ee8a8f07994383579e79f
Hannes Mehnert <hannes@mehnert.org>
2020-08-01 18:18:08 +0200

    monitoring and syslog for deployment

--- config.ml
+++ config.ml
@@ -6,19 +6,38 @@ let keys =
   let doc = Key.Arg.info ~doc:"nsupdate keys (name:type:value,...)" ["keys"] in
   Key.(create "keys" Arg.(opt (list string) [] doc))
 
+let monitor =
+  let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+  Key.(create "monitor" Arg.(opt (some ip_address) None doc))
+
+let syslog =
+  let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+  Key.(create "syslog" Arg.(opt (some ip_address) None doc))
+
+let name =
+  let doc = Key.Arg.info ~doc:"Name of the unikernel" ["name"] in
+  Key.(create "name" Arg.(opt string "sn.nqsb.io" doc))
+
 let dns_handler =
   let packages =
     [
       package "logs" ;
       package ~min:"5.0.0" ~sublibs:["mirage"] "dns-server";
       package "dns-tsig";
+      package "mirage-monitoring";
+      package ~sublibs:["mirage"] ~min:"0.3.0" "logs-syslog";
     ]
-  and keys = Key.([ v keys ])
+  and keys = [
+    Key.v keys ;
+    Key.v name ; Key.v monitor ; Key.v syslog
+  ]
   in
   foreign
     ~keys
     ~packages
-    "Unikernel.Main" (random @-> pclock @-> mclock @-> time @-> stackv4v6 @-> job)
+    "Unikernel.Main" (console @-> random @-> pclock @-> mclock @-> time @-> stackv4v6 @-> stackv4v6 @-> job)
+
+let management_stack = generic_stackv4v6 ~group:"management" (netif ~group:"management" "management")
 
 let () =
-  register "secondary" [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4v6 default_network ]
+  register "secondary" [dns_handler $ default_console $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4v6 default_network $ management_stack ]
--- unikernel.ml
+++ unikernel.ml
@@ -1,8 +1,18 @@
 (* (c) 2017, 2018 Hannes Mehnert, all rights reserved *)
-module Main (R : Mirage_random.S) (P : Mirage_clock.PCLOCK) (M : Mirage_clock.MCLOCK) (T : Mirage_time.S) (S : Tcpip.Stack.V4V6) = struct
+module Main (C : Mirage_console.S) (R : Mirage_random.S) (P : Mirage_clock.PCLOCK) (M : Mirage_clock.MCLOCK) (T : Mirage_time.S) (S : Tcpip.Stack.V4V6) (Management : Tcpip.Stack.V4V6) = struct
   module D = Dns_server_mirage.Make(P)(M)(T)(S)
 
-  let start _rng _pclock _mclock _ s =
+  module Monitoring = Mirage_monitoring.Make(T)(P)(Management)
+  module Syslog = Logs_syslog_mirage.Udp(C)(P)(Management)
+
+  let start c _rng _pclock _mclock _ s management =
+    let hostname = Key_gen.name () in
+    (match Key_gen.syslog () with
+     | None -> Logs.warn (fun m -> m "no syslog specified, dumping on stdout")
+     | Some ip -> Logs.set_reporter (Syslog.create c management ip ~hostname ()));
+    (match Key_gen.monitor () with
+     | None -> Logs.warn (fun m -> m "no monitor specified, not outputting statistics")
+     | Some ip -> Monitoring.create ~hostname ip management);
     let keys = List.fold_left (fun acc str ->
         match Dns.Dnskey.name_key_of_string str with
         | Error (`Msg msg) -> Logs.err (fun m -> m "key parse error %s" msg) ; exit 64
